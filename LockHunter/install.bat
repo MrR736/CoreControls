e755a6a17475fc451874e93f8f72ca631c6357f7
MrR736 <mrr6451@outlook.com>
2024-08-23 20:41:08 -0700

    Update install.bat

--- LockHunter/install.bat
+++ LockHunter/install.bat
@@ -1,6 +1,5 @@
 @echo off
 cd /d %~dp0
-
 @Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter" /VE /D "LockHunter" /F >Nul
 @Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter" /V "Icon" /D "\"%CD%\Icon.ico\"
 @Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter\command" /VE /D "\"%CD%\LockHunter.exe\" \"%%1\"" /F >Nul
