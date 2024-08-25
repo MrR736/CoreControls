0a239ddb8f21c1e0705d08b417e45327dfb97fea
MrR736 <mrr6451@outlook.com>
2024-08-25 07:58:03 -0700

    Add files via upload

--- CV/CheckLastVersion.cmd
+++ CV/CheckLastVersion.cmd
@@ -1,12 +1,5 @@
 @echo off
-cd /d %~dp0
-copy ".\msv.txt" ".\Temp"
-copy ".\lv.txt" ".\Temp"
-cd .\Temp
+cd %core%\CoreControls\CV\Temp
 wget https://raw.githubusercontent.com/MrR736/Core/main/CV.cmd
-cd /d %~dp0
-cls
-timeout /t 2 /nobreak
-cd .\Temp
-type msv.txt >> TCV.cmd
+type msv.txt >> "%core%\CoreControls\CV\Temp\TCV.cmd"
 exit
