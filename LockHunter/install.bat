4b4b60e5f93f1c58cdfdb6712b89f3e7bf4d08a4
MrR736 <mrr6451@outlook.com>
2024-09-05 17:38:23 -0700

    Add files via upload

--- LockHunter/install.bat
+++ LockHunter/install.bat
@@ -1,10 +1,10 @@
 @echo off
 cd /d %~dp0
 @Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter" /VE /D "LockHunter" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter" /V "Icon" /f /D "%CD%\Icon.ico\"
+@Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter" /V "Icon" /f /D "%CD%\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\*\shell\LockHunter\command" /VE /D "\"%CD%\LockHunter.exe\" \"%%1\"" /F >Nul
 
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\LockHunter" /VE /D "LockHunter" /F >Nul
-@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\LockHunter" /V "Icon" /f /D "%CD%\Icon.ico\"
+@Reg Add "HKEY_CLASSES_ROOT\Directory\shell\LockHunter" /V "Icon" /f /D "%CD%\Icon.ico"
 @Reg Add "HKEY_CLASSES_ROOT\Directory\shell\LockHunter\command" /VE /D "\"%CD%\LockHunter.exe\" \"%%1\"" /F >Nul
 exit
