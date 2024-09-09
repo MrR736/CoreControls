a7d30c7d79b78394a095b2dcdd393412cffa0570
MrR736 <mrr6451@outlook.com>
2024-09-09 01:02:58 -0700

    Add files via upload

--- CCo/CCo.cmd
+++ CCo/CCo.cmd
@@ -1,10 +1,68 @@
 @echo off
+title UpdateCore /CCo
+
+REM Server to be pinged
+SET server=github.com
+REM Size of packet to be send to server (bytes)
+SET packetSize=1
+REM Network info
+SET netSSID=[SSID]
+SET netName=[Network Name]
+SET netInterface=[Network Interface]
+REM Pause time between each network check (seconds)
+SET successfulTimeout=10
+REM Pause time after reconnection before next check (seconds)
+SET failedTimeout=5
+REM Write failed network connections to log (Boolean)
+SET writeToLog=1
+
+REM Do not change
+SET lastFail=never
+SET successfulRepetitions=0
+
+REM Check internet connection
+:check
+cls
+ECHO Pinging %server%...
+PING -n 1 -l %packetSize% %server% >NUL
+IF %errorlevel% EQU 0 GOTO successful
+GOTO failed
+
+REM Internet connection succeeded
+:successful
+title UpdateCore /CCo
+color FA
 cd "%ProgramData%\PhoenixOS"
 mkdir Update
-cd %ProgramData%\PhoenixOS\Update
+cd "%ProgramData%\PhoenixOS\Update"
 wget https://github.com/MrR736/CoreCommands/archive/refs/heads/main.zip
 7za x main.zip -o"%ProgramData%\PhoenixOS\Update"
 del /s /q "%ProgramData%\PhoenixOS\Update\main.zip"
+del /s /q "%ProgramData%\PhoenixOS\Core\CoreCommands"
 xcopy /s /y "%ProgramData%\PhoenixOS\Update\CoreCommands-main" "%ProgramData%\PhoenixOS\Core\CoreCommands"
 rd /s /q "%ProgramData%\PhoenixOS\Update"
+exit
+
+REM Internet connection failed
+:failed
+title UpdateCore /CCo
+Color F4
+cls 
+ECHO Sorry But You Need to Enable Your Internet To Download CoreCommands
+ECHO:
+ECHO Reasons For Connection to The Internet to Download The Latest Version Of CoreCommands
+ECHO:
+set /P c=Do You Want To Check Your Internet Again[Y/N]?
+if /I "%c%" EQU "Y" goto :y
+if /I "%c%" EQU "y" goto :y
+if /I "%c%" EQU "N" goto :n
+if /I "%c%" EQU "n" goto :n
+ECHO Indecisive.
+pause
+goto failed
+
+:y
+GOTO check
+
+:n
 exit
